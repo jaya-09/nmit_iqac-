bcf3e45bb869d00a495b6a36917fad1b2d8ad69c
Kalyan <kalyanb2000@gmail.com>
2022-02-28 07:57:00 +0530

    data storage correction

--- process2.php
+++ process2.php
@@ -1,5 +1,6 @@
 <?php
     error_reporting(0);
+    session_start();
 
     $field270 = $_POST["16a"];
 
@@ -109,19 +110,17 @@
     $field338 = $_POST["28a"];
 
 
-
-
- $keys = array($field1, 
-$csv_line = $keys;
- foreach( $keys as $key ){
-     array_push($csv_line,'' . $_GET[$key]);
-
+$temp = $_SESSION["csv_line"];
+ $_SESSION['keys2'] = array($field270,$field271,$field272,$field273,$field274,$field275,$field276,$field277,$field278,$field279,$field280,$field281,$field282,$field283,$field284,$field285,$field286,$field287,$field288,$field289,$field290,$field291,$field292,$field293,$field294,$field295,$field296,$field297,$field298,$field299,$field300,$field301,$field302,$field303,$field304,$field305,$field306,$field307,$field308,$field309,$field310,$field311,$field312,$field313,$field314,$field315,$field316,$field317,$field318,$field319,$field320,$field321,$field322,$field323,$field324,$field325,$field326,$field327,$field328,$field329,$field330,$field331,$field332,$field333,$field334,$field335,$field336,$field337,$field338); 
+ $_SESSION['csv_line'] = array_merge($temp,$_SESSION['keys2']);
+ foreach($_SESSION['keys2'] as $key ){
+     array_push($_SESSION['csv_line'],'' . $_GET[$key]);
+        
  }
  $fname = 'data.csv'; 
  if(!file_exists($fname)){$csv_line = $csv_line."\r\n" ;}
  $fcon = fopen($fname,'a');
- fputcsv($fcon,$fcontent);
+fputcsv($fcon,$_SESSION['csv_line']);
  fclose($fcon);
 
-//  header("Location: ./table2.php");
  ?>
