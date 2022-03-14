e5b9b71e5d8963d2e154dee69a0ea602a4f97fc9
Blazeleo <satviksams@gmail.com>
2022-03-14 20:46:00 +0530

    minor fixes

--- index.php
+++ index.php
@@ -9,8 +9,6 @@
 		$output = $data;
 		if (is_array($output))
 			$output = implode(',', $output);
-	
-		// echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
 	}
 ?>
 
@@ -22,85 +20,77 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <title>login_page</title>
-    <link rel="stylesheet" href="style.css">
+    <title>Login</title>
+    <link rel="stylesheet" href="./homepage.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
       integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   </head>
 
-  <!--<body style="background-color: #1b2a47;">-->
-
-  <body style="
-      background-image: url('nmitbgcropped1.jpg');
+  <body 
+  style="background-image: url('./nmitbgcropped1.jpg');
   background-repeat: no-repeat;
   background-attachment: fixed;  
   background-size: cover;">
+  <!--Banner-->
+  <div class="mycontainer">
+  <img src="nmitlogo.jpeg" width="100%" height="130vh">
+  </div>
 
-    <div>
-      <div class="flex-container">
-        <div id="logo">
-          <img src="nmitlogo.jpeg" width="1920px" height="150px">
+    <!--Images-->
+
+  <div class="mycontainer">
+    <div class="itemcont">
+
+      <div class="card">
+      <img class="card-img-top" src="Prof. N R Shetty, Advisor (1).jpg" alt="Card image cap" class="rounded-lg">
+      <br>
+      <h3>Prof. N R Shetty,<br> Advisor</h3>
+      </div>
+      
+      <div class="card">
+        <img class="card-img-top" src="HCN.jpg" alt="Card image cap" class="rounded-lg"> <br>
+        <h3>Dr H C Nagaraj,<br> Principal</h3>
+      </div>
+
+    </div>
+
+    <div class="itemcont2">
+
+      <div class="Logfields">
+        <h1 id="Iq" style="color: aliceblue;">IQAC </h1>
+        <form action="./login.php" method="post">
+        <p style="font-size: 40px; color: white;  margin-top:1vh;filter: drop-shadow(10px 10px 10px black); ">
+        <b>NMIT-AAC Faculty Audit</b></p>
+
+        <div class="input-group has-validation">
+          <span class="input-group-text" id="validationTooltipUsernamePrepend">@</span>
+          <input type="text" class="form-control" id="validationTooltipUsername" name="uname"
+          aria-describedby="validationTooltipUsernamePrepend" required placeholder="UserName" required="true">
+          <div class="invalid-tooltip">Please choose a unique and valid username.</div>
         </div>
-        <div class="flex-container">
-          <div class="card" style="width: 24rem;">
-            <img class="card-img-top" src="Prof. N R Shetty, Advisor (1).jpg" alt="Card image cap" class="rounded-lg">
-            <br>
-            <h3> &nbsp &nbsp Prof. N R Shetty, Advisor</h3><br>
 
-          </div>
-          <div class="card" style="width: 24rem;">
-            <img class="card-img-top" src="HCN.jpg" alt="Card image cap" class="rounded-lg"> <br>
-            <h3> &nbsp &nbsp Dr H C Nagaraj, Principal</h3><br>
+        <input type="password" class="form-control" name="password" placeholder="Password" required="true" />
+        <button class="btn btn-lg btn-primary btn-block" input type="submit"role="button">Login</button>
+        </form>
+      </div>
+      <h4 id="names" style="color: white;"> Created by Jaya Shiva Darshini, Satvik A, Kalyan B, Lasya Sistla<br> Department of ISE </h4>
 
-          </div>
-          <div class="cards" style="width: 40rem;">
-            <div class="wrapper">
-              <!-- <form class="login">
-                <form class="loginpage"> -->
-                  <form action="./login.php" method="post">
-                      <!--<h2 id="Iq" class="form-signin-heading">IQAC </h2> -->
-                      <h5 id="Iq" class="form-signin-heading" style="color: aliceblue;">IQAC 
-					  <!-- ?php echo "<script>console.log('Debug Objects: " . implode(',', $result) . "' );</script>"?> -->
-					</h5>
-                      <p style="font-size: 40px; color: white;  filter: drop-shadow(10px 10px 10px black); ">&nbsp &nbsp
-                        &nbsp &nbsp <b>NMIT-AAC Faculty Audit</b></p>
+    </div>
 
-                      <div class="col-md-13 position-relative">
+    <div class = "itemcont3">
 
-                        <div class="input-group has-validation">
-                          <span class="input-group-text" id="validationTooltipUsernamePrepend">@</span>
-                          <input type="text" class="form-control" id="validationTooltipUsername" name="uname"
-                            aria-describedby="validationTooltipUsernamePrepend" required placeholder="UserName" required="true">
-                          <div class="invalid-tooltip">
-                            Please choose a unique and valid username.
-                          </div>
-                        </div>
-                      </div> <br>
-                      <!-- <input type="username" class="form-control" name="uname" placeholder="Username" required="true" /><br> -->
-                      <input type="password" class="form-control" name="password" placeholder="Password" required="true" />
+      <div class="card">
+      <img class="card-img-top" src="Mr.Rohit Punja.jpg" alt="Card image cap" class="rounded-lg"> <br>
+        <h3>Mr.Rohit Punja,<br>Administrator</h3>
+      </div>
+      
+      <div class="card">
+        <img class="card-img-top" src="Dr V Sridhar, Dean.jpg" alt="Card image cap" class="rounded-lg"> <br>
+        <h3> Dr V Sridhar,<br>Dean</h3>
+      </div>
 
-                      <!---- < a button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>  -->
-                      <button class="btn btn-lg btn-primary btn-block" input type="submit"
-                        role="button">Login</button>
-                  </form>
-            </div>
-          </div>
-          <div class="flex-container2" style="height: 10%;">
-            <div class="card" style="width: 24rem; ">
-              <img class="card-img-top" src="Mr.Rohit Punja.jpg" alt="Card image cap" class="rounded-lg"> <br>
-              <h3>Mr.Rohit Punja,Administrator</h3><br>
+    </div>
+  </div>
 
-
-
-            </div>
-            <div class="card" style="width: 24rem;">
-              <img class="card-img-top" src="Dr V Sridhar, Dean.jpg" alt="Card image cap" class="rounded-lg"> <br>
-              <h3> &nbsp &nbsp &nbsp &nbsp Dr V Sridhar,Dean</h3><br>
-
-
-            </div>
-          </div>
-        </div>
-        <h4 id="names" style="color: white;padding: 45rem 0rem 0rem  45rem; "> Created by Jaya Shiva Darshini, Satvik A, Kalyan B, Lasya Sistla<br> Department of ISE </h4>
   </body>
 </html>
---
+++ indexold.php
@@ -0,0 +1,98 @@
+<?php 
+	require "db_conn.php";
+	session_start();
+	
+	$sql = "SELECT * from users";
+	$result = mysqli_query($conn,$sql);
+	$result = mysqli_fetch_array($result);
+	function console_log($data) {
+		$output = $data;
+		if (is_array($output))
+			$output = implode(',', $output);
+	
+		// echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+	}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>login_page</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+      integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  </head>
+
+  <!--<body style="background-color: #1b2a47;">-->
+
+  <body style="
+      background-image: url('nmitbgcropped1.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;  
+  background-size: cover;">
+
+    <div>
+      <div class="flex-container">
+        <div id="logo">
+          <img src="nmitlogo.jpeg" width="1920px" height="150px">
+        </div>
+        <div class="flex-container">
+          <div class="card" style="width: 24rem;">
+            <img class="card-img-top" src="Prof. N R Shetty, Advisor (1).jpg" alt="Card image cap" class="rounded-lg">
+            <br>
+            <h3> &nbsp &nbsp Prof. N R Shetty, Advisor</h3><br>
+
+          </div>
+          <div class="card" style="width: 24rem;">
+            <img class="card-img-top" src="HCN.jpg" alt="Card image cap" class="rounded-lg"> <br>
+            <h3> &nbsp &nbsp Dr H C Nagaraj, Principal</h3><br>
+
+          </div>
+          <div class="cards" style="width: 40rem;">
+            <div class="wrapper">
+                  <form action="./login.php" method="post">
+                      <h5 id="Iq" class="form-signin-heading" style="color: aliceblue;">IQAC </h5>
+                      <p style="font-size: 40px; color: white;  filter: drop-shadow(10px 10px 10px black); ">&nbsp &nbsp
+                        &nbsp &nbsp <b>NMIT-AAC Faculty Audit</b></p>
+
+                      <div class="">
+
+                        <div class="input-group has-validation">
+                          <span class="input-group-text" id="validationTooltipUsernamePrepend">@</span>
+                          <input type="text" class="form-control" id="validationTooltipUsername" name="uname"
+                            aria-describedby="validationTooltipUsernamePrepend" required placeholder="UserName" required="true">
+                          <div class="invalid-tooltip">
+                            Please choose a unique and valid username.
+                          </div>
+                        </div>
+                      </div> <br>
+                      <input type="password" class="form-control" name="password" placeholder="Password" required="true" />
+                      <button class="btn btn-lg btn-primary btn-block" input type="submit"
+                        role="button">Login</button>
+                  </form>
+            </div>
+          </div>
+          <div class="flex-container2" style="height: 10%;">
+            <div class="card" style="width: 24rem; ">
+              <img class="card-img-top" src="Mr.Rohit Punja.jpg" alt="Card image cap" class="rounded-lg"> <br>
+              <h3>Mr.Rohit Punja,Administrator</h3><br>
+
+
+
+            </div>
+            <div class="card" style="width: 24rem;">
+              <img class="card-img-top" src="Dr V Sridhar, Dean.jpg" alt="Card image cap" class="rounded-lg"> <br>
+              <h3> &nbsp &nbsp &nbsp &nbsp Dr V Sridhar,Dean</h3><br>
+
+
+            </div>
+          </div>
+        </div>
+        <h4 id="names" style="color: white;padding: 45rem 0rem 0rem  45rem; "> Created by Jaya Shiva Darshini, Satvik A, Kalyan B, Lasya Sistla<br> Department of ISE </h4>
+  </body>
+</html>
