79cc7188b25b3314e692215e876f5a297292d501
Kalyan <kalyanb2000@gmail.com>
2022-03-17 22:00:27 +0530

    update query 2.0

--- table1.php
+++ table1.php
@@ -12,6 +12,7 @@ if($_SESSION['loggedin']!='TRUE'){
 
 if($_GET['id'] != NULL){
   $query = 'select * from answers1 where ansid="'.$_GET['id'].'"';
+  $_SESSION['ansid'] = $_GET['id'];
   $cntQry = "select count(*) from information_schema.columns where table_name='answers1'";
   $retval = mysqli_query($conn, $query);
   $cntvl = mysqli_query($conn, $cntQry);
@@ -735,13 +736,13 @@ if($_GET['id'] != NULL){
 
             <td class="tg-0pky">
               <div class="form-check">
-                <input class="form-check-input" type="radio" name="4ac3" id="4ac3">
+                <input class="form-check-input" type="radio" name="4ac3" id="4ac3" value="option1">
                 <label class="form-check-label" for="4ac3">
                   YES
                 </label>
               </div>
               <div class="form-check">
-                <input class="form-check-input" type="radio" name="4ac3" id="4ac33">
+                <input class="form-check-input" type="radio" name="4ac3" id="4ac33" value="option2">
                 <label class="form-check-label" for="4ac33">
                   NO
                 </label>
